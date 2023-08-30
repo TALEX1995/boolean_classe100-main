9c73a44974a36daf70f84b9456dcff3cf6f16d7b
Deltafortal <aldolucchetta21@gmail.com>
2023-08-30 15:29:52 +0200

    Added basic elimination script

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -32,7 +32,7 @@
             <td>{{ $student->surname }}</td>
             <td class="d-flex align-items-center justify-content-end gap-2">
               <a class="btn btn-warning" href="#">Modifica</a>
-              <form action="{{ route('students.destroy', $student)}}" method="POST" id="delete-form">
+              <form action="{{ route('students.destroy', $student)}}" method="POST" class="delete-form">
                 @csrf
                 @method('DELETE')
                 <button class="btn btn-danger">Elimina</button>
@@ -43,6 +43,6 @@
       </tbody>
     </table>
   </div>
-
+  @vite('resources/js/delete-confirm.js')
 @endsection
 
