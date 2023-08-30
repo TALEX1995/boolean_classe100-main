afff6f4393a7a78acc7265f1523f9d463157d11d
TALEX1995 <tanase.alex@libero.it>
2023-08-30 16:12:10 +0200

    Hotfix delete modal

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -4,56 +4,56 @@
 
 @section('main')
 
-    <div class="container">
+  <div class="container">
 
 
 
 
-        <!-- Delete Message -->
-        @include('includes.allerts')
+    <!-- Delete Message -->
+    @include('includes.allerts')
 
-        <div class="d-flex justify-content-end">
+    <div class="d-flex justify-content-end">
 
-            <a class="btn btn-primary mt-3" href="{{ route('students.create') }}">Crea</a>
+      <a class="btn btn-primary mt-3" href="{{ route('students.create') }}">Crea</a>
 
-
-        </div>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Cognome</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($students as $student)
-                    <tr>
-                        <th>{{ $student->name }}</th>
-                        <td>{{ $student->surname }}</td>
-                        <td class="d-flex align-items-center justify-content-end gap-2">
-
-                            <a class="btn btn-warning" href="{{ route('students.edit', $student) }}">Modifica</a>
-                      
-                            <form action="{{ route('students.destroy', $student) }}" method="POST" id="delete-form">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-danger">Elimina</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
     </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Nome</th>
+          <th scope="col">Cognome</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($students as $student)
+          <tr>
+            <th>{{ $student->name }}</th>
+            <td>{{ $student->surname }}</td>
+            <td class="d-flex align-items-center justify-content-end gap-2">
+
+              <a class="btn btn-warning" href="{{ route('students.edit', $student) }}">Modifica</a>
+
+              <form action="{{ route('students.destroy', $student) }}" method="POST" class="delete-form"
+                data-bs-toggle="modal" data-bs-target="#deleteModal" data-name="{{ $student->name }}">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger">Elimina</button>
+              </form>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 
 
   <!-- Modale -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="deleteModal" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Eliminazione Studente</h1>
+          <h1 class="modal-title fs-5">Eliminazione Studente</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
